9afe35a2d34fc03607ccfdef3e16e796c704e033
RiotonZong <a4214679@pccu.edu.tw>
2020-02-14 17:57:02 +0800

    bug:unsafe url need to  fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { EnvService } from './../services/env.service';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  imgBase64: string;
+  imgBase64: string | null;
 
-  constructor(private envService: EnvService) {}
+  constructor(
+    private envService: EnvService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
+    try {
+      sessionStorage.removeItem('scanIMG');
+    } catch (e) {}
+
     this.envService.Get_ScannedImage().subscribe(res => {
+      if (res !== null) {
+        this.sanitizer.bypassSecurityTrustUrl(res);
+      }
       this.imgBase64 = res;
     });
     this.envService.Get_PrintStatu().subscribe(res => {
--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 
+declare var scanner: any;
+
 @Injectable({
   providedIn: 'root',
 })
 export class EnvService {
-  scanner: any = (window as any).scanner;
+  // scanner: any = (window as any).scanner;
 
-  imgBase64: BehaviorSubject<string>;
+  imgBase64: BehaviorSubject<string | null>;
   printStatu: BehaviorSubject<boolean>;
 
   constructor() {
@@ -16,7 +18,9 @@ export class EnvService {
   }
 
   public Process_ScantoImage() {
-    this.scanner.scan(this.Process_StoreImg, {
+    sessionStorage.removeItem('scanIMG');
+    this.Set_ScannedImage();
+    scanner.scan(this.Process_StoreImg, {
       twain_cap_setting: {
         ICAP_PIXELTYPE: 'TWPT_RGB', // Color
         ICAP_XRESOLUTION: '100',
@@ -28,33 +32,41 @@ export class EnvService {
         {
           type: 'return-base64',
           format: 'jpg',
-          jpeg_quality: '100',
         },
       ],
     });
   }
 
-  private Process_StoreImg(successful: any, mesg: string | null, res: any) {
-    console.log(successful);
-    console.log(mesg);
-    console.log(JSON.parse(res).output[0].result[0]);
-
-    if (successful) {
-      try {
-        this.Set_ScannedImage(JSON.parse(res).output[0].result[0]);
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
+  public Process_StoreImg(successful: any, mesg: string | null, res: any) {
+    if (!successful) {
       console.error('Failed: ' + mesg);
+      return;
     }
+
+    if (
+      successful &&
+      mesg != null &&
+      mesg.toLowerCase().indexOf('user cancel') >= 0
+    ) {
+      // User cancelled.
+      console.error('User cancelled');
+      return;
+    }
+
+    const scannedImages = scanner.getScannedImages(res, true, false);
+    sessionStorage.setItem('scanIMG', scannedImages[0].src);
   }
 
-  private Set_ScannedImage(res: any): void {
-    this.imgBase64.next(res);
+  public Set_ScannedImage(): void {
+    const detectScanImg = setInterval(() => {
+      this.imgBase64.next(sessionStorage.getItem('scanIMG'));
+      if (this.imgBase64.value !== null) {
+        clearInterval(detectScanImg);
+      }
+    }, 100);
   }
 
-  public Get_ScannedImage(): Observable<string> {
+  public Get_ScannedImage(): Observable<string | null> {
     return this.imgBase64.asObservable();
   }
 
